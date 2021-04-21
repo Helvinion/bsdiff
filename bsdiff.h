769adb0cad29c5badab92520a80f48a6126e61fe
Quentin Mignot <qmignot@gmail.com>
2021-04-21 19:45:53 +0200

    Change indentation method to spaces

--- bsdiff.h
+++ bsdiff.h
@@ -33,11 +33,11 @@
 
 struct bsdiff_stream
 {
-	void* opaque;
+    void* opaque;
 
-	void* (*malloc)(size_t size);
-	void (*free)(void* ptr);
-	int (*write)(struct bsdiff_stream* stream, const void* buffer, int size);
+    void* (*malloc)(size_t size);
+    void (*free)(void* ptr);
+    int (*write)(struct bsdiff_stream* stream, const void* buffer, int size);
 };
 
 int bsdiff(const uint8_t* old, int64_t oldsize, const uint8_t* new, int64_t newsize, struct bsdiff_stream* stream);
